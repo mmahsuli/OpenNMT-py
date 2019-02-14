5d9a8df1250f372f7f6cef6e3dce266cd7124bfd
Mohammad Mahdi Mahsuli <mmahsuli@gmail.com>
2019-02-14 18:03:37 +0330

    Added the code for training LSTM length model

---
+++ train_length_model.py
@@ -0,0 +1,21 @@
+import torch
+import onmt.utils.length_model
+import configargparse
+import onmt.opts as opts
+
+
+if __name__ == "__main__":
+    parser = configargparse.ArgumentParser(
+        description='length_model.py',
+        config_file_parser_class=configargparse.YAMLConfigFileParser,
+        formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
+    opts.config_opts(parser)
+    opts.add_md_help_argument(parser)
+    opts.length_model_opts(parser)
+
+    opt = parser.parse_args()
+
+    checkpoint = torch.load(opt.mt_model,
+                            map_location=lambda storage, loc: storage)
+    vocab = checkpoint['vocab'][1][1]
+    onmt.utils.length_model.train(opt, vocab)
